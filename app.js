7a00b70ada996c6aec5b0df5d0520a8e7f904ab2
Rudra-Deshmukh <88197680+Rudra-Deshmukh@users.noreply.github.com>
2021-08-12 18:16:46 +0530

    aD

---
+++ app.js
@@ -0,0 +1,21 @@
+function checkpwdpat(){
+    var pwd=document.getElementById("pwd").value;
+    var pattern=/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_])[\w!@#$%^&*]{6,}$/;
+    if(pattern.test(pwd)==false){
+        document.getElementById("wpwd").style.display="inline-block";
+        document.getElementById("pwd").value="";
+    }
+    else{
+        document.getElementById("wpwd").style.display="none";
+    }
+
+}
+
+function reg(){
+    if(document.getElementById("uname").value=="" || document.getElementById("email").value=="" || document.getElementById("pwd").value==""){
+      document.getElementById("succes").style.display="none";  
+    }
+    else{
+        document.getElementById("succes").value="inline-block";
+    }
+}
